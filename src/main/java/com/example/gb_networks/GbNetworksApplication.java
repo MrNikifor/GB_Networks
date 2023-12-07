a4871f027749845e71a1062afce668d507dd7afc
osian <oisa.nikifor@gmail.com>
2023-12-07 17:27:58 +0300

    Lesson1

---
+++ src/main/java/com/example/gb_networks/GbNetworksApplication.java
@@ -0,0 +1,13 @@
+package com.example.gb_networks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbNetworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbNetworksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gb_networks/GbNetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gb_networks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbNetworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
